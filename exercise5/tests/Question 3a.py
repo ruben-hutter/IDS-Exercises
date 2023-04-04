aa623cff640ce1c568f10665ad98419600d73f4e
ruben-hutter <hutter.ruben@gmail.com>
2023-04-04 13:34:56 +0200

    created repo and added sheets until 6

---
+++ exercise5/tests/Question 3a.py
@@ -0,0 +1,11 @@
+OK_FORMAT = True
+
+test = {   'name': 'Question 3a',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> assert_equal(len(y), n_samples)\n', 'hidden': False, 'locked': False},
+                                   {'code': '>>> assert_almost_equal(y.mean(), 2.963 , places=2)\n', 'hidden': False, 'locked': False},
+                                   {'code': '>>> assert_almost_equal(y.std() , 1.975 , places=2)\n', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
